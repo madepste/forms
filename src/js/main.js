43a628f76b8146e45cfa9ac89989f83f3f942ade
madepste <madepste@gmail.com>
2014-12-03 14:18:52 -0500

    added js

---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+(function($,W,D)
+{
+    var JQUERY4U = {};
+     
+         JQUERY4U.UTIL =
+	     {setupFormValidation: function()
+	             {
+		                form validation rules
+				            $("#register-form").validate({
+				              rules: {
+					      firstname: "required",
+					                          lastname: "required",
+								                      email: {
+										                              required: true,
+													                              email: true
+																                          },
+																			   password: {
+																			                           required: true,
+																						                           minlength: 5
+																									                       },
+																											        agree: "required"
+																												                },
+																														 messages: {
+																														                     firstname: "Please enter your firstname",
+																																                         lastname: "Please enter your lastname",
+																																			                     password: {
+																																					     required: "Please provide a password",
+																																					                             minlength: "Your password must be at least 5 characters long"
+																																								                         },
+																																											  email: "Please enter a valid email address",
+																																											                      agree: "Please accept our policy"
+																																													                      },
+																																															       submitHandler: function(form) {
+																																															                           form.submit();
+																																																		                   }
+																																																				               });
+																																																					               }
+																																																						           }
